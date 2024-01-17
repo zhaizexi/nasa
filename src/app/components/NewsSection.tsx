ca141938e41cb866ca2ae7932f6f3337e2ba7f64
zhaizexi <499327776@qq.com>
2024-01-17 16:03:46 +0800

    fix: home-latestNews-限制激活状态鼠标悬浮样式

--- src/app/components/NewsSection.tsx
+++ src/app/components/NewsSection.tsx
@@ -53,31 +53,32 @@ export default function NewsSection(props: Props) {
           {news.map((news, index) => (
             <SwiperSlide
               key={index}
-              className="pr-10 group -translate-x-1/2 cursor-pointer"
+              className="pr-10 group -translate-x-1/2 select-none"
             >
               <div className="block group-[.swiper-slide-active]:hidden group-[.swiper-slide-next]:hidden absolute w-full h-full bg-white/60 z-10"></div>
-              <div className="bg-gray-dark relative mb-6 overflow-hidden lg:mb-10 aspect-video">
-                <Image
-                  className="transition-all duration-300 scale-105 delay-200 group-hover:scale-100"
-                  src={news.img}
-                  alt={news.title}
-                  priority={false}
-                  unoptimized
-                  fill={true}
-                />
-              </div>
-              <div className="transition-all duration-300 delay-200  group-hover:-translate-y-5">
-                <div className="flex flex-wrap">
-                  <p className="text-subtitle divide-gray-mid flex divide-x mb-4">
-                    <span>{news.subject}</span>{" "}
-                    <span className="sr-only">.</span>
+              <div className="group/item cursor-pointer">
+                <div className="bg-gray-dark relative mb-6 overflow-hidden lg:mb-10 aspect-video">
+                  <Image
+                    className="transition-all duration-300 scale-105 delay-200 group-hover/item:scale-100"
+                    src={news.img}
+                    alt={news.title}
+                    priority={false}
+                    unoptimized
+                    fill={true}
+                  />
+                </div>
+                <div className="transition-all duration-300 delay-200  group-hover/item:-translate-y-5">
+                  <div className="flex flex-wrap">
+                    <p className="text-subtitle divide-gray-mid flex divide-x mb-4">
+                      <span>{news.subject}</span>{" "}
+                      <span className="sr-only">.</span>
+                    </p>
+                  </div>{" "}
+                  <p className="text-gray-dark text-xl font-medium leading-tight tracking-tight lg:text-3xl">
+                    {news.title}
                   </p>
-                </div>{" "}
-                <p className="text-gray-dark text-xl font-medium leading-tight tracking-tight lg:text-3xl">
-                  {news.title}
-                </p>
-              </div>
-              <div className="absolute bottom-0 text-jpl-red -translate-x-4 delay-200 opacity-0 group-hover:translate-x-0 group-hover:opacity-100 text-2xl leading-normal transition duration-300 ">
+                </div>
+                <div className="absolute bottom-0 text-jpl-red -translate-x-4 delay-200 opacity-0 group-hover/item:translate-x-0 group-hover/item:opacity-100 text-2xl leading-normal transition duration-300 ">
                   <svg
                     width="26"
                     height="16"
@@ -94,6 +95,7 @@ export default function NewsSection(props: Props) {
                     ></path>
                   </svg>
                 </div>
+              </div>
             </SwiperSlide>
           ))}
         </Swiper>
